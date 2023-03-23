b0441f5e2b6cac301ef7360781e99cf7c127f1cc
akplfc221 <100085162+akplfc221@users.noreply.github.com>
2023-03-23 12:14:46 +0530

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+
+    >
+      <div class="d-flex align-center">
+        <v-img 
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/123.jpg"
+          transition="scale-transition"
+          width="40"
+        />
+      
+       
+
+
+
+        {{ $const.APP_NAME }}
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+      >
+        <span class="mr-2">Login</span>
+        <v-icon>mdi-account-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+@import url('@/assets/styles.css');
+</style>
